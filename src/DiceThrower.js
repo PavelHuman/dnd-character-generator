a2775f42202e219fc68723afab837e665adb1ea1
Dmitry G. Anderson <dandgerson@gmail.com>
2022-03-08 23:40:49 +0300

    class expanation

--- src/DiceThrower.js
+++ src/DiceThrower.js
@@ -1,4 +1,8 @@
 class DiceThrower {
+  constructor() {
+    this.name = 'DiceThrower'
+  }
+
   static parseDices(dices) {
     const [qtyDice, typeOfDices] = dices.split('d')
     return [qtyDice, typeOfDices]
@@ -13,6 +17,30 @@ class DiceThrower {
   static throwDice(diceType) {
     return Math.ceil(Math.random() * diceType)
   }
+}
+
+/*
+  *************
+*/
+
+
+function DiceThrowerEs5() {
+  this.name = 'DiceThrower'
+}
+
+DiceThrowerEs5.parseDices = function (dices) {
+  const [qtyDice, typeOfDices] = dices.split('d')
+  return [qtyDice, typeOfDices]
+}
+
+DiceThrowerEs5.throwDice = function (diceType) {
+  return Math.ceil(Math.random() * diceType)
+}
+
+DiceThrowerEs5.prototype.throwDices = function (dicesToTrow) {
+  const [diceQty, diceType] = DiceThrowerEs5.parseDices(dicesToTrow)
+
+  return Array.from({ length: +diceQty }, () => DiceThrowerEs5.throwDice(+diceType))
 }
 
 export default DiceThrower
--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import DiceThrower from './DiceThrower.js'
+import { Dog, DogEs5 } from './Dog.js'
 
-console.log("index.js")
+const dog = new Dog()
+const dogEs5 = new DogEs5()
+
+console.dir(Dog)
+console.log('instance Dog', dog)
+
+console.log('')
+console.log('*******')
+console.log('')
+console.dir(DogEs5)
+console.log('instance DogEs5', dogEs5)
+
+console.log(dog instanceof DogEs5)
+console.log(dogEs5 instanceof DogEs5)
+
+// console.log("index.js")
+
+// console.dir(DiceThrower)
 
 const diceThrower = new DiceThrower()
+
+// console.log('instance DiceThrower', diceThrower)
 
 // Бросьте четыре 6-гранных кости
 const thrown4d6 = diceThrower.throwDices('4d6')
 
-console.log({
-  thrown4d6,
-})
+// console.log({
+//   thrown4d6,
+// })
 
 /**
  * запишите сумму трёх наибольших результатов.
@@ -24,18 +44,18 @@ console.log({
 // https://learn.javascript.ru/array-methods#sort-fn
 const sorted = thrown4d6.slice().sort((a, b) => a - b)
 
-console.log({
-  sorted,
-})
+// console.log({
+//   sorted,
+// })
 
 // 2. получить первые три наибольших элемента
 const firstMaxThree = sorted.reverse().slice(0, 3)
 
-console.log({
-  firstMaxThree,
-})
+// console.log({
+//   firstMaxThree,
+// })
 
 // 3. получить их сумму
 const sum = firstMaxThree.reduce((acc, current) => acc + current, 0)
 
-console.log({ sum })
+// console.log({ sum })
