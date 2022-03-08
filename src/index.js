7391b4a1f69e3df4be45b2dc5278153b66d6610d
Dmitry G. Anderson <dandgerson@gmail.com>
2022-03-09 00:08:06 +0300

    clean code

--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
 import DiceThrower from './DiceThrower.js'
 import Entity from './Entity.js'
 
-// class Explanation Start
-
-import { Dog, DogEs5 } from './Dog.js'
-
-const dog = new Dog()
-const dogEs5 = new DogEs5()
-
-console.dir(Dog)
-console.log('instance Dog', dog)
-
-console.log('')
-console.log('*******')
-console.log('')
-console.dir(DogEs5)
-console.log('instance DogEs5', dogEs5)
-
-console.log(dog instanceof DogEs5)
-console.log(dogEs5 instanceof DogEs5)
-
-// class Explanation End
-
-// ***************
-
 console.log("index.js")
 
 const diceThrower = new DiceThrower()
 
 // Бросьте четыре 6-гранных кости
-const thrown4d6 = diceThrower.throwDices('4d6')
+const thrown4d6 = diceThrower.throwDices('4d6') // 4d6 (4 6-гранных кости)
 
-// console.log({
-//   thrown4d6,
-// })
+console.log({
+  thrown4d6,
+})
 
 const entity = new Entity()
 entity.sayHello()
@@ -51,18 +28,18 @@ entity.sayHello()
 // https://learn.javascript.ru/array-methods#sort-fn
 const sorted = thrown4d6.slice().sort((a, b) => a - b)
 
-// console.log({
-//   sorted,
-// })
+console.log({
+  sorted,
+})
 
 // 2. получить первые три наибольших элемента
 const firstMaxThree = sorted.reverse().slice(0, 3)
 
-// console.log({
-//   firstMaxThree,
-// })
+console.log({
+  firstMaxThree,
+})
 
 // 3. получить их сумму
 const sum = firstMaxThree.reduce((acc, current) => acc + current, 0)
 
-// console.log({ sum })
+console.log({ sum })
