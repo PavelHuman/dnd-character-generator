b9131f7ebe8a49ba210a3b6858bbe135dc975579
Dmitry G. Anderson <dandgerson@gmail.com>
2022-03-12 21:39:17 +0300

    render entity level info

--- src/components/Character.js
+++ src/components/Character.js
@@ -11,8 +11,14 @@ class Character extends Component {
 
     const template = `
       <h2>Generated Character</h2>
-      <div>Name: ${this.model.entity.name}</div>
-      <div>Race: ${this.model.entity.race}</div>
+
+      <div>
+        <div>Name: ${this.model.entity.name}</div>
+        <div>Race: ${this.model.entity.race}</div>
+        <div>Level: ${this.model.entity.level}</div>
+        <div>Experience Points: ${this.model.entity.experiencePoints}</div>
+        <div>Proficiency Bonus: ${this.model.entity.proficiencyBonus}</div>
+      </div>
     `
 
     this.root.insertAdjacentHTML('afterbegin', template)
