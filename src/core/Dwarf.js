a556d87d32dc4ecb203e40afda531972d689c924
PavelHuman <pavelhuman1@gmail.com>
2022-03-15 18:25:08 +0300

    constructor with racial traits

--- src/core/Dwarf.js
+++ src/core/Dwarf.js
@@ -1,3 +1,14 @@
 class Dwarf {
 
+  constructor(
+    abilityScoreIncrease, age, alignment, size, speed, languages, subraces
+  ) {
+    this.abilityScoreIncrease = abilityScoreIncrease,
+      this.age = age,
+      this.alignment = alignment,
+      this.size = size,
+      this.speed = speed,
+      this.languages = languages,
+      this.subraces = subraces
+  }
 }
