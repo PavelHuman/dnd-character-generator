aedb150065acf32b610d0628f3db7abd764e94ef
Dmitry G. Anderson <dandgerson@gmail.com>
2022-03-15 23:25:05 +0300

    add DwarfMixin & add applyMixins

---
+++ src/core/races/dwarf/index.js
@@ -0,0 +1,1 @@
+export { DwarfMixin } from './DwarfMixin.js'
--- src/core/races/index.js
+++ src/core/races/index.js
@@ -1,8 +1,13 @@
-import Dwarf from './Dwarf.js'
+import { DwarfMixin } from './dwarf/index.js'
+import Race from './Race.js'
+
+const applyMixins = (Base, DefaultMixin, ...mixins) => [DefaultMixin, ...mixins].reduce((acc, mixin) => mixin(acc), Base)
 
 export const raceFactory = (race, options) => {
   switch (race) {
-    case 'dwarf': return new Dwarf(options)
+    case 'dwarf': {
+      return new (applyMixins(Race, DwarfMixin))(options)
+    }
     default: return {}
   }
 }
