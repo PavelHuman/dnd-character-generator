b343e6276e54cd8e690de0671ac80fa58ca7082f
PavelHuman <pavelhuman1@gmail.com>
2022-03-16 21:51:57 +0300

    add propertis

--- src/core/races/Dwarf.js
+++ src/core/races/Dwarf.js
@@ -1,25 +1,46 @@
 class Dwarf {
-  abilityScoreIncrease = 2
+
+  abilityScoreIncrease = { constitution: 2 }
+
   age = 200
+
   alignment = 'fair'
+
   size = 140
+
   speed = 25
-  languages = true
+
+  languages = ['common', 'dwarvish']
+
   subraces = ['hill', 'mountain']
 
-  constructor(
-    darkvision, dwarvenResilience, dwarvenCombatTraining,
-    proficiency, stonecunning, subrace,
-    dwarvenToughness, dwarvenArmorTraining
-  ) {
-    this.darkvision = darkvision,
-      this.dwarvenResilience = dwarvenResilience,
-      this.dwarvenCombatTraining = dwarvenCombatTraining,
-      this.proficiency = { tools: [] },
-      this.stonecunning = stonecunning,
-      this.subrace = subrace,
-      this.dwarvenToughness = dwarvenToughness,
-      this.dwarvenArmorTraining = dwarvenArmorTraining
+  darkvision = {
+    state: true,
+    description: 'Accustomed to life underground, uoy have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You cant discern color in darkness, only shades of gray.',
+    title: 'Darkvision'
   }
+
+  dwarvenResilience = {
+    state: true,
+    description: 'You have advantage on saving throws against poison, and you have resistance against poison damage (explained in chapter 9, "Combat")'
+  }
+
+  dwarvenCombatTraining = true
+
+  proficiency = {
+    tools: ['blacksmith'],
+  }
+
+  stonecunning = {
+    state: true,
+    description: 'Whenever you make an Intelligence History check related to the origin of stonework, you are considered proficient in the History skill and add double your proficiency bonus to the check, instead of your normal proficiency bonus',
+  }
+
+  subrace
+
+  dwarvenToughness
+
+  dwarvenArmorTraining
+
 }
 export const dwarf = new Dwarf()
