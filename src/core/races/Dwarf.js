1bdeceeeeea9f4f902424a2a3c89012bccb6fc5c
PavelHuman <pavelhuman1@gmail.com>
2022-03-16 12:13:51 +0300

    rewriting properties

--- src/core/races/Dwarf.js
+++ src/core/races/Dwarf.js
@@ -8,21 +8,20 @@ class Dwarf {
   subraces
 
   constructor() {
-    this.initSubrace
-    this.abilityScoreIncrease = { title: string, description: string, state: boolean }
-    this.age = { title: string, description: string, state: boolean }
-    this.alignment = { title: string, description: string, state: boolean }
-    this.size = { title: string, description: string, state: boolean }
-    this.speed = { title: string, description: string, state: boolean }
-    this.darkvision = { title: string, description: string, state: boolean }
-    this.dwarvenResilience = { title: string, description: string, state: boolean }
-    this.dwarvenCombatTraining = { title: string, description: string, state: boolean }
+    this.abilityScoreIncrease = abilityScoreIncrease
+    this.age = age
+    this.alignment = alignment
+    this.size = size
+    this.speed = speed
+    this.darkvision = darkvision
+    this.dwarvenResilience = dwarvenResilience
+    this.dwarvenCombatTraining = dwarvenCombatTraining
     this.proficiency = { tools: [] }
-    this.stonecunning = { title: string, description: string, state: boolean }
-    this.languages = { title: string, description: string, state: boolean }
-    this.subrace = { title: string, description: string, state: boolean }
-    this.dwarvenArmorTraining = { title: string, description: string, state: boolean }
-    this.dwarvenToughness = { title: string, description: string, state: boolean }
+    this.stonecunning = stonecunning
+    this.languages = languages
+    this.subrace = subrace
+    this.dwarvenArmorTraining = dwarvenArmorTraining
+    this.dwarvenToughness = dwarvenToughness
 
   }
 }
