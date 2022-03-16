7fe54c3b2698e6394d31bc674c3d8cb633a36b34
PavelHuman <pavelhuman1@gmail.com>
2022-03-16 18:44:24 +0300

    value assignment

--- src/core/races/Dwarf.js
+++ src/core/races/Dwarf.js
@@ -1,31 +1,25 @@
 class Dwarf {
-  abilityScoreIncrease
-  age
-  alignment
-  size
-  speed
-  languages
-  subraces
+  abilityScoreIncrease = 2
+  age = 200
+  alignment = 'fair'
+  size = 140
+  speed = 25
+  languages = true
+  subraces = ['hill', 'mountain']
 
   constructor(
-    abilityScoreIncrease, age, alignment, size, speed, darkvision, dwarvenResilience,
-    dwarvenCombatTraining, stonecunning, languages, subrace,
-    dwarvenArmorTraining, dwarvenToughness
+    darkvision, dwarvenResilience, dwarvenCombatTraining,
+    proficiency, stonecunning, subrace,
+    dwarvenToughness, dwarvenArmorTraining
   ) {
-    this.abilityScoreIncrease = abilityScoreIncrease,
-      this.age = age,
-      this.alignment = alignment,
-      this.size = size,
-      this.speed = speed,
-      this.darkvision = darkvision,
+    this.darkvision = darkvision,
       this.dwarvenResilience = dwarvenResilience,
       this.dwarvenCombatTraining = dwarvenCombatTraining,
       this.proficiency = { tools: [] },
       this.stonecunning = stonecunning,
-      this.languages = languages,
       this.subrace = subrace,
-      this.dwarvenArmorTraining = dwarvenArmorTraining,
-      this.dwarvenToughness = dwarvenToughness
+      this.dwarvenToughness = dwarvenToughness,
+      this.dwarvenArmorTraining = dwarvenArmorTraining
   }
 }
 export const dwarf = new Dwarf()
