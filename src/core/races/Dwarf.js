71953ea1a050a7b9f57286c1d819b2ae8810b739
PavelHuman <pavelhuman1@gmail.com>
2022-03-16 11:43:45 +0300

    property proficiency

--- src/core/races/Dwarf.js
+++ src/core/races/Dwarf.js
@@ -8,6 +8,7 @@ class Dwarf {
   subraces
 
   constructor() {
+    this.initSubrace
     this.abilityScoreIncrease = { title: string, description: string, state: boolean }
     this.age = { title: string, description: string, state: boolean }
     this.alignment = { title: string, description: string, state: boolean }
@@ -16,10 +17,12 @@ class Dwarf {
     this.darkvision = { title: string, description: string, state: boolean }
     this.dwarvenResilience = { title: string, description: string, state: boolean }
     this.dwarvenCombatTraining = { title: string, description: string, state: boolean }
-    this.toolProficiency = { title: string, description: string, state: boolean }
+    this.proficiency = { tools: [] }
     this.stonecunning = { title: string, description: string, state: boolean }
     this.languages = { title: string, description: string, state: boolean }
     this.subrace = { title: string, description: string, state: boolean }
     this.dwarvenArmorTraining = { title: string, description: string, state: boolean }
+    this.dwarvenToughness = { title: string, description: string, state: boolean }
+
   }
 }
