1b8c03c64eb37c334fefcdb5f3562a9c73d9c7c6
Dmitry G. Anderson <dandgerson@gmail.com>
2022-03-17 22:59:33 +0300

    udpate congigs

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    'src/**/*.{js,jsx}',
+    'src/**',
   ],
 
   // The directory where Jest should output its coverage files
