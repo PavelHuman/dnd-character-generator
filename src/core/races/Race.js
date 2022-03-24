53edc0b20dfa3622777f2b1193ba8e64a4125009
PavelHuman <pavelhuman1@gmail.com>
2022-03-24 23:40:41 +0300

    methods initAge, initSize

--- src/core/races/Race.js
+++ src/core/races/Race.js
@@ -17,13 +17,31 @@ export class Race {
   }
 
   constructor(options = {}) {
+    this.initProficiency(options.proficiency)
     this.initAge(options.age)
+    this.initSize(options.size)
+    this.initAlignment(options.alignment)
   }
 
   initAge(age) {
-    this.age = age ?? 18
+    if (this.age <= 49) {
+      this.age = age ?? 50 + Math.ceil(Math.random() * (400 - 50))
+    }
+  }
+
+  initSize(size) {
+    if (this.size.value <= 65) {
+      this.size.value = size ?? 66 + Math.ceil(Math.random() * (244 - 66))
+    }
+  }
+  initProficiency(proficiency) {
+
+  }
+  initAlignment(alignment) {
+
   }
 }
+
 
 export const race = new Race()
 console.log({ race })
