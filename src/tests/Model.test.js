fbb8faa5d71cf14472a58fb2d9eec0d5f4eab449
Dmitry G. Anderson <dandgerson@gmail.com>
2022-04-01 10:36:31 +0300

    add test cases

---
+++ src/tests/Model.test.js
@@ -0,0 +1,20 @@
+import Model from '../model/Model.js'
+import Entity from '../Entity.js'
+
+const model = new Model()
+
+describe('model', () => {
+  test('should be instance of Model', () => {
+    expect(model).toBeInstanceof(Model)
+  })
+
+  test('should has entity property', () => {
+    expect(model).toHaveProperty('entity', expect.any(Object))
+  })
+
+  describe('entity', () => {
+    test('should be instance of Entity', () => {
+      expect(model.entity).toBeInstanceof(Entity)
+    })
+  })
+})
