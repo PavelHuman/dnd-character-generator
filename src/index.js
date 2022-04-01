1825f08c36162b605f8b5d672ecd700f3f6982a1
PavelHuman <pavelhuman1@gmail.com>
2022-04-01 22:02:57 +0300

    console.log(model) in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import { dwarf } from './core/races/Dwarf.js'
-import Entity from './Entity.js'
+import Entity from './model/Entity.js'
 import { race } from './core/races/Race.js'
 import { hillDwarf } from './core/races/HillDwarf.js'
-
+import { Model } from '../model/Model.js'
 
 console.log(hillDwarf)
 console.log(race)
 console.log(dwarf)
 
-const entity = new Entity()
+const model = new Model()
+console.log(model)
 
+const entity = new Entity()
 console.log(entity)
