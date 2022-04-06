1355e6336cc23bbff580a916d18ed4f3143767cf
PavelHuman <pavelhuman1@gmail.com>
2022-04-06 14:41:25 +0300

    add php.getRaceTraits

--- src/core/PlayersHandBook.js
+++ src/core/PlayersHandBook.js
@@ -1,4 +1,6 @@
 import { diceRoller as diceRoller } from './DiceRoller.js'
+import { HillDwarf } from './races/HillDwarf.js'
+import { MountainDwarf } from './races/MountainDwarf.js'
 class PlayersHandBook {
   getAbilityScore() {
     const rolln4d6 = diceRoller.rollDices('4d6')
@@ -15,6 +17,16 @@ class PlayersHandBook {
       intelligence: null,
       wisdom: null,
       charisma: null,
+    }
+  }
+  getRacialTraits(race, options) {
+    if (race === 'dwarf') {
+      if (options.subrace === 'hill dwarf') {
+        return new HillDwarf(options)
+      }
+      if (options.subrace === 'mountain dwarf') {
+        return new MountainDwarf(options)
+      }
     }
   }
 }
--- src/model/Model.js
+++ src/model/Model.js
@@ -2,7 +2,12 @@ import Entity from './Entity.js'
 
 export class Model {
 
-  entity = new Entity()
+  entity = new Entity({
+    race: 'dwarf',
+    options: {
+      subrace: 'hill dwarf',
+    },
+  })
 
 }
 export const model = new Model()
